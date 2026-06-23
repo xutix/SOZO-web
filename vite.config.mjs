30c0e75ba32c0f69e9d4be39372e0d787da7eda0
Xutic <92292096+xutix@users.noreply.github.com>
2026-06-23 14:12:33 +0800

    Launch SOZO Tech website

---
+++ vite.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+
+export default defineConfig({
+  optimizeDeps: {
+    include: ["react", "react-dom/client"],
+  },
+  server: {
+    warmup: {
+      clientFiles: ["./src/main.jsx"],
+    },
+  },
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(import.meta.dirname, "index.html"),
+        products: resolve(import.meta.dirname, "products.html"),
+        dock: resolve(import.meta.dirname, "dock.html"),
+        fpv: resolve(import.meta.dirname, "fpv.html"),
+        cases: resolve(import.meta.dirname, "cases.html"),
+        about: resolve(import.meta.dirname, "about.html"),
+        contact: resolve(import.meta.dirname, "contact.html"),
+      },
+    },
+  },
+});
